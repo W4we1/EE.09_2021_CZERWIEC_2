4cb415bafeebf72bdba2ff0006f860d264d696c6
W4we1 <Wawel2004@gmail.com>
2023-04-19 15:16:10 +0200

    Praca w szkole

--- video.php
+++ video.php
@@ -29,9 +29,54 @@
             $username="root";
             $password="";
             $dbname="dane3";
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            if (!$conn) {
+                die("Połączenie nieudane: " . mysqli_connect_error());
+            }
+            $sql = "SELECT id, nazwa, opis, zdjecie FROM produkty WHERE id IN (18, 22, 23, 25)";
+            $result = mysqli_query($conn, $sql);
+            if (mysqli_num_rows($result)>0) {
+                while($row=mysqli_fetch_assoc($result)) {
+                    echo "<div class='film'>";
+                    echo "<h4>" . $row["id"] . ". " . $row["nazwa"] . "</h4>";
+                    echo "<img src='" . $row["zdjecie"] . "' alt='film'>";
+                    echo "<p>" . $row["opis"] . "</p>";
+                    echo "</div>";
+                }
+            }
+            else {
+                echo "Brak wyników";
+            }
+            mysqli_close($conn);
+            ?>
         </div>
         <div id="list-block">
         <h3>Filmy Fantastyczne</h3>
+                        <?php
+            $servername="localhost";
+            $username="root";
+            $password="";
+            $dbname="dane3";
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            if (!$conn) {
+                die("Połączenie nieudane: " . mysqli_connect_error());
+            }
+            $sql = "SELECT id, nazwa, opis, zdjecie FROM produkty WHERE Rodzaje_id = 12;";
+            $result = mysqli_query($conn, $sql);
+            if (mysqli_num_rows($result)>0) {
+                while($row=mysqli_fetch_assoc($result)) {
+                    echo "<div class='film'>";
+                    echo "<h4>" . $row["id"] . ". " . $row["nazwa"] . "</h4>";
+                    echo "<img src='" . $row["zdjecie"] . "' alt='film'>";
+                    echo "<p>" . $row["opis"] . "</p>";
+                    echo "</div>";
+                }
+            }
+            else {
+                echo "Brak wyników";
+            }
+            mysqli_close($conn);
+            ?>
         </div>
         <div id="footer">
         <p>Stronę wykonał: 0000000000OZ</p>
