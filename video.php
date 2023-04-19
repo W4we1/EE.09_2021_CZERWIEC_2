c466b31932c45bbc1f94ebd47e6836b78c712e95
W4we1 <Wawel2004@gmail.com>
2023-04-19 10:09:59 +0200

    Praca w szkole

---
+++ video.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Video On Demand</title>
+    <link rel="stylesheet" type="text/css" href="styl3.css">
+    </head>
+    <body>
+    <div id="left-banner">
+        <h1>Internetowa wypożyczalnia filmów</h1>
+        </div>
+        <div id="right-banner">
+            <table>
+            <tr>
+            <th><font color="white">Kryminał</font></th>
+            <th><font color="white">Horror</font></th>
+            <th><font color="white">Przygodowy</font></th>
+            </tr>
+            <tr>
+            <th><font color="white">20</font></th>
+            <th><font color="white">30</font></th>
+            <th><font color="white">20</font></th>
+            </tr>
+            </table>
+        </div>
+        <div id="list-block">
+        <h3>Polecamy</h3>
+            <?php
+            $servername="localhost";
+            $username="root";
+            $password="";
+            $dbname="dane3";
+        </div>
+        <div id="list-block">
+        <h3>Filmy Fantastyczne</h3>
+        </div>
+        <div id="footer">
+        <p>Stronę wykonał: 0000000000OZ</p>
+        </div>
+    </body>
+</html>
